41afb7b6749b39a3128b67da762f9fa2c379026f
ishaghevariya <ishaghevariya.dcs22n@vnsgu.ac.in>
2022-03-31 18:19:57 +0530

    --Add Registration Layout--

--- EventBookingApp/wwwroot/js/site.js
+++ EventBookingApp/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function(){
+    var placeholderelement = $('placeholder');
+    $('button[data-toggle="ajax-modal"]').click(function (event) {
+        var url = $(this).data('url')
+        $.get(url).done(function (data) {
+            placeholderelement.html(data);
+            placeholderelement.find('.modal').modal('show');
+        })
+    })
+})
